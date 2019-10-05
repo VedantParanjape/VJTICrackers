a87b8da0949f487b393edf7f94e8fd76042c0b11
Bhavya-sheth <sheth.bhavya06@gmail.com>
2019-10-05 15:51:09 +0530

    login/signup done. no file upload

--- VJTICrackersBlog/routes.py
+++ VJTICrackersBlog/routes.py
@@ -18,17 +18,19 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template("signup.html")
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('home'))
+    # form = RegistrationForm()
+    # if form.validate_on_submit():
+    #     hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+    #     user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+    #     db.session.add(user)
+    #     db.session.commit()
+    #     flash('Your account has been created! You are now able to log in', 'success')
+    #     return redirect(url_for('login'))
+    # return render_template('register.html', title='Register', form=form)
+
 
 
 @app.route("/login", methods=['GET', 'POST'])
