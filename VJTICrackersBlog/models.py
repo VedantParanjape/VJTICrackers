24fa3f9e213c15e60e3f2e96e7c31e26d4d30da9
akshatshah21 <akshatshah_21@yahoo.in>
2019-10-05 12:00:19 +0530

    UI branch

--- VJTICrackersBlog/models.py
+++ VJTICrackersBlog/models.py
@@ -11,10 +11,9 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    # image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     #posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return str.format("User('{self.username}', '{self.email}', '{self.image_file}')")
-
+        return "{}, {}".format(self.username, self.email)
